7356244b9394ede8742a5c49f064afa4434a3a71
magnusguldager <m@magnusg.dk>
2024-12-18 21:19:25 +0100

    remove hardcoded jobs and unnecessary elements to make this publishable

--- scripts.js
+++ scripts.js
@@ -13,6 +13,11 @@ function job(jobName, pay, pension, bonus, fritvalg, hours, phone = Boolean, int
 
 var inputToLocaleStringElm = document.querySelectorAll(".format-input")
 
+var paidPhoneValue = 150
+var paidInternetValue = 250
+let lunchValue = 40
+let workingDays = 175
+
 // auto format inputs with toLocaleString while typing
 inputToLocaleStringElm.forEach(function(elm){
     elm.addEventListener('input', function(){
@@ -73,10 +78,11 @@ jobForm.onsubmit = function(event){
 let jobInfoContainer = document.querySelector(".jobs-info")
 
 function showJobInfo(){
+    jobInfoContainer.innerHTML = ""; // Clear the container
 
     function createJobDetailElement(className, description = '') {
         const detailDiv = document.createElement('div');
-        detailDiv.className = `${className}-div`
+        detailDiv.className = `${className}-div delay-div`
         detailDiv.innerHTML = `${description}`;
         return detailDiv;
     }
@@ -90,14 +96,19 @@ function showJobInfo(){
     `Din arbejdsgiver indbetaler <span class="highlight">${pension.toLocaleString("da-DK")}%</span> til din pension, som årligt svarer til <span class="highlight">${pensionTotal.toLocaleString("da-DK")}</span> kroner.`
     ));
 
-    jobInfoContainer.appendChild(createJobDetailElement("bonus", 
-    `Udover din årlige løn, får du bonus eller provision på <span class="highlight">${bonus.toLocaleString("da-DK")} kroner</span>. Det antages, at dette beløb ikke er pensionsgivende.`
-    ));
+    if(bonus < 0){
+        jobInfoContainer.appendChild(createJobDetailElement("bonus", 
+        `Udover din årlige løn, får du bonus eller provision på <span class="highlight">${bonus.toLocaleString("da-DK")} kroner</span>. Det antages, at dette beløb ikke er pensionsgivende.`
+        ));
+    }
 
-    fritvalgTotal = pay * fritvalg / 100
-    jobInfoContainer.appendChild(createJobDetailElement("fritvalg", 
-    `Din fritvalgsordning giver dig <span class="highlight">${fritvalgTotal.toLocaleString("da-DK")} kroner</span> ekstra i løn om året. Det antages, at dette beløb ikke er pensionsgivende.`
-    ));
+    if(fritvalg < 0){
+        fritvalgTotal = pay * fritvalg / 100
+        jobInfoContainer.appendChild(createJobDetailElement("fritvalg", 
+        `Din fritvalgsordning giver dig <span class="highlight">${fritvalgTotal.toLocaleString("da-DK")} kroner</span> ekstra i løn om året. Det antages, at dette beløb ikke er pensionsgivende.`
+        ));
+    }
+
 
     monthlyPay = pay / 12
     regulatePay = monthlyPay * 37 / hours
@@ -110,14 +121,65 @@ function showJobInfo(){
         `Du arbejder <span class="highlight">${hours} timer</span> om ugen og tjener <span class="highlight">${monthlyPay.toLocaleString("da-DK")} kroner</span> om måneden. Hvis din arbejdstid var 37 timer om ugen, ville det svare til <span class="highlight">${regulatePay.toLocaleString("da-DK")}</span> kroner om måneden.`
         ));
     }
+
+
+    if(internet && phone){
+        jobInfoContainer.appendChild(createJobDetailElement("internet",
+        `Din arbejdsgiver betaler dit internet og telefon. Det hæver din samlede lønpakke med et konservativt beløb på <span class="highlight">${paidInternetValue + paidPhoneValue} kroner</span> om måneden (før skat), som dækker over de udgifter du ville have til internetabonnement, telefonabonnement og udskiftning af ny telefon.`
+        ))
+    } else if (internet && !phone) {
+        jobInfoContainer.appendChild(createJobDetailElement("internet",
+        `Din arbejdsgiver betaler dit internet. Det hæver din samlede lønpakke med et konservativt beløb på <span class="highlight">${paidInternetValue} kroner</span> om måneden (før skat), som dækker over de udgifter du ville have til internetabonnement.`
+        ))
+    } else if (!internet && phone) {
+        jobInfoContainer.appendChild(createJobDetailElement("internet",
+        `Din arbejdsgiver betaler din telefon. Det hæver din samlede lønpakke med et konservativt beløb på <span class="highlight">${paidPhoneValue} kroner</span> om måneden (før skat), som dækker over de udgifter du ville have til telefonabonnement og udskiftning af ny telefon. Da du i forvejen bliver beskattet af fri telefon, vil du ikke blive beskattet af betalt internet. Overvej at tage det op til næste lønsamtale.`
+        ))
+    } else {
+        jobInfoContainer.appendChild(createJobDetailElement("internet",
+        `Du får hverken betalt telefon eller internet. Hvis du kan få adgang til din arbejdsplads' internet hjemmefra, kan dit arbejde betale dit internet uden du bliver beskattet. Overvej at bringe det op til næste lønsamtale.`
+        ))
+    }
+
+
+    let lunchValueTotal = lunchValue * workingDays
+    lunch ? jobInfoContainer.appendChild(createJobDetailElement("lunch",
+    `Din frokostordning øger din lønpakke med <span class="highlight">${lunchValueTotal.toLocaleString("da-DK")} kroner</span> om året, antaget at du arbejder ${workingDays} dage om året og frokosten koster ${lunchValue} kr om dagen.`
+    )) :
+    ""
+
+
+    exDaysOffValue = pay * 0.0045 * 5
+    exDaysOff ? 
+    jobInfoContainer.appendChild(createJobDetailElement("extra-days-off",
+    `Dine 5 feriefridage har en årlig værdi af <span class="highlight">${exDaysOffValue.toLocaleString("da-DK")} kroner</span>.`
+    )) :
+    ""
+
+    const jobDivs = document.getElementsByClassName(".delay-div")
+    console.log(jobDivs)
+
+    Array.from(document.getElementsByClassName('delay-div'), e => e.innerText)
+
+
+    // jobDivs.forEach(function(){
+    //     var i = 0;
+    //     var txt = this.innerText
+    //     var speed = 50; /* The speed/duration of the effect in milliseconds */
+    //     console.log(this.innerText)
+    //     console.log(txt)
+    //     console.log("hi")
+    // });
+
 }
 
-let job1 = new job("Nordicals", 396000, 5, 9900, 5, "37", true, false, true, false);
-let job2 = new job("HLTV", 480000, 5, 10000, 0, "37,5", false, true, false, false);
-let job3 = new job("HLTVferiedage", 480000, 5, 10000, 0, "37,5", false, true, false, true);
+// testjobs
+// let job1 = new job("Nordicals", 396000, 5, 9900, 5, "37", true, false, true, false);
+// let job2 = new job("HLTV", 480000, 5, 10000, 0, "37,5", false, true, false, false);
+// let job3 = new job("HLTVferiedage", 480000, 5, 10000, 0, "37,5", false, true, false, true);
 
 
-let jobsArray = [job1, job2, job3];
+let jobsArray = [];
 
 // Get reference to the div where you want to display the jobs
 let jobsContainer = document.querySelector(".jobs-wrap");
@@ -198,6 +260,9 @@ function updatejobs() {
         totalGross += job.pay * job.fritvalg / 100
         totalGross += job.bonus
         job.exDaysOff ? totalGross += job.pay * 0.0045 * 5 : totalGross += 0;
+        job.phone ? totalGross += paidPhoneValue * 12 : totalGross += 0;
+        job.internet ? totalGross += paidInternetValue * 12 : totalGross += 0;
+        job.lunch ? totalGross += lunchValue * workingDays : totalGross += 0;
         return totalGross.toFixed(0)
     }
     
